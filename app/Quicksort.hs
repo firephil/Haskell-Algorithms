87d7c256173ca5a4c81079d9deaf82274c9c7ead
firephil <philip@firesoft.gr>
2021-03-15 18:21:52 +0200

    code cleanup

--- app/Quicksort.hs
+++ app/Quicksort.hs
@@ -1,6 +1,6 @@
 import Data.List (partition)
 
--- one line implementation
+  -- one line implementation
 
 qsort1 [] = []
 qsort1 (x:xs) = qsort1 [y | y <- xs, y < x] ++ [x] ++ qsort1 [y | y <- xs, y >= x]
@@ -11,26 +11,19 @@ qsort (x:xs) = qsort ys ++ x : qsort zs
   where
     (ys, zs) = partition (< x) xs
 
--- simple implementation using 3 lists
+-- simple implementation using 3 lists  left,pivot,right
 
 quicksort :: [Int] -> [Int]
 quicksort [] = []
 quicksort list  = quicksort left ++ pivotlist ++ quicksort right
   where
-    --pivot :: Int
-    pivot = head list
-
-    --pivotlist :: [Int]
-    pivotlist = filter( == pivot) list
-
-    --left :: [Int]
-    left = filter(< pivot) list
-
-   -- right :: [Int]
-    right = filter(> pivot) list
+     pivot = head list
+     pivotlist = filter( == pivot) list
+     left = filter(< pivot) list
+     right = filter(> pivot) list
 
 main :: IO ()
-
 main = do
-        putStrLn $ show  (quicksort [5,2,3,1,4,0,2])
-        line <- getLine -- wait for a key press to exit
+        print(quicksort [5,2,2,3,3,1,4,0,2])
+        getLine -- wait for a key press to exit
+        print("Press a enter to exit")
