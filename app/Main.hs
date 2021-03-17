4dcbb7acf1f16700e06726df11969838f19bf46d
firephil <philip@firesoft.gr>
2021-03-17 22:12:24 +0200

    added Insertion sort

--- app/Main.hs
+++ app/Main.hs
@@ -6,17 +6,25 @@ import Control.Monad.Random
 
 import ShuffleList(shuffle)
 import STshuffle(st_shuffle)
+import InsertionSort(insertionSort)
 
-ls = [1..8000]
+ls = [1..800]
 rev = reverse ls
 --xs = st_shuffle ls
 
 
 main :: IO ()
 main =  do
-
+          --print( take(10) (insertionSort rev) ) test insertion sort works
           values <- evalRandIO(shuffle ls)  -- store Random[[Int]] to values list (unwrap monad)
+          putStrLn("Sorting a Randomized list...")
           execute (quicksort values)
           execute (mergeSort values)
+          execute (insertionSort values)
+
+          putStrLn("")
+          putStrLn("Sorting a reverse sorted list...")
+
           execute (quicksort rev) -- pure performance when the list is reversed 0(n^2)
           execute (mergeSort rev) -- merge sort is a stable sort O(n(log(n)))
+          execute (insertionSort rev)
